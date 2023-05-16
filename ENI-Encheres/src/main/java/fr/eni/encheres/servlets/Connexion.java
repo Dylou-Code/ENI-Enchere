c26aa0d322612c3948d06b40657ce8acd2f6c055
Dylan CEPRIKA <dylan.ceprika2022@campus-eni.fr>
2023-05-16 09:38:00 +0200

    fix:Second init Project

--- ENI-Encheres/src/main/java/fr/eni/encheres/servlets/Connexion.java
+++ ENI-Encheres/src/main/java/fr/eni/encheres/servlets/Connexion.java
@@ -27,7 +27,7 @@ public class Connexion extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		request.getRequestDispatcher("/WEB-INF/jsp/connexion.jsp").forward(request, response);
+		request.getRequestDispatcher("/WEB-INF/jsp/connexion.js").forward(request, response);
 	}
 
 	/**
